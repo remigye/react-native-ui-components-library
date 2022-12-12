10222e15ba498dddda8b2eea1b2131e0da22b76a
Rémi GYEJACQUOT <r.gyejacquot@maatdata.fr>
2022-12-12 15:37:19 +0100

    docs(fonts): load font for expo example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,42 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { Title } from 'react-native-ui-components-library';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'Arial-RoundedExtraBold': require('../../src/assets/fonts/Arial-RoundedExtraBold.ttf'),
+  });
+
+  React.useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Title>Title component</Title>
-    </View>
+    <SafeAreaView style={styles.safe} onLayout={onLayoutRootView}>
+      <View style={styles.container}>
+        <Title>Title component</Title>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safe: { flex: 1 },
   container: {
     flex: 1,
     alignItems: 'center',
